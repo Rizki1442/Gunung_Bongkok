1f6726c4c75a3ae882df16f3d76d762afdd64030
Rizki1442 <117267456+Rizki1442@users.noreply.github.com>
2025-02-10 16:47:19 -0800

    Add files via upload

---
+++ asep irawan/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/panduan_dasar')
+def panduan_dasar():
+    return render_template('panduan_dasar.html')
+
+@app.route('/lokasi')
+def lokasi():
+    return render_template('lokasi.html')
+
+@app.route('/galery')
+def galery():
+    return render_template('galery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
