c5f6d7fa5a161d083960f4b34dcc0a54ae80a8b5
Rizki1442 <117267456+Rizki1442@users.noreply.github.com>
2025-02-13 20:34:28 -0800

    Update app.py

--- asep irawan/app.py
+++ asep irawan/app.py
@@ -26,5 +26,9 @@ def lokasi():
 def galery():
     return render_template('galery.html')
 
+@app.route('/erd')
+def erd():
+    return render_template('erd.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
